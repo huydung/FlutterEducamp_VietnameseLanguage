51817afbc56fb44b9504530bc8d34dc33bedfd02
huydung <contact.huydung@gmail.com>
2020-09-24 11:56:57 +0700

    Finish the Layout Design for Vietnamese Vowel (ep4).

---
+++ flutter_edu_vietnamese/android/app/src/main/kotlin/com/huydung/flutter_edu_vietnamese/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.huydung.flutter_edu_vietnamese
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- flutter_edu_vietnamese/lib/ep4.dart
+++ flutter_edu_vietnamese/lib/ep4.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,26 +6,13 @@ void main() {
 }
 
 class VowelsApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red[900],
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.dark,
       ),
       home: MyHomePage(title: 'Vietnamese Vowels'),
     );
@@ -40,50 +28,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  FlatButton buildButton({String character, Color color}){
-    return FlatButton(
-      color: color,
-      onPressed: () => {},
-      textColor: Colors.white70,
-      child: Text(character,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
+  List<String> vowels = [
+    'a',
+    'ă',
+    'â',
+    'e',
+    'ê',
+    'i',
+    'o',
+    'ô',
+    'ơ',
+    'u',
+    'ư',
+    'y'
+  ];
+  List<Color> colors = [
+    Colors.deepOrange[900],
+    Colors.deepOrange[800],
+    Colors.deepOrange[700],
+    Colors.pink[900],
+    Colors.pink[800],
+    Colors.pink[700],
+    Colors.red[900],
+    Colors.red[800],
+    Colors.red[700],
+    Colors.amber[900],
+    Colors.amber[800],
+    Colors.amber[700],
+  ];
 
-        ),
-      )
+  Widget buildLayout() {
+    const int COLS = 3;
+    const int ROWS = 4;
+    int i;
+    List<Widget> rowWidgets = new List<Widget>();
+    for (var r = 0; r < ROWS; r++) {
+      List<Widget> cells = [];
+      for (var c = 0; c < COLS; c++) {
+        i = r * COLS + c;
+        print('Index: $i');
+        FlatButton button = FlatButton(
+            color: colors[i],
+            onPressed: () => {},
+            textColor: Colors.white70,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.zero)),
+            child: Text(
+              vowels[i].toUpperCase() + ' ' + vowels[i],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Fira Sans Extra Condensed',
+                fontSize: 60,
+                //fontWeight: FontWeight.bold,
+              ),
+            ));
+        cells.add(Expanded(child: button));
+      }
+      ;
+      rowWidgets.add(new Expanded(
+          child: Row(
+        children: cells,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+      )));
+    }
+    return Column(
+      children: rowWidgets,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    List<String> vowels = [
-      'a', 'ă', 'â',
-      'e', 'ê', 'i',
-      'o', 'ô', 'ơ',
-      'u', 'ư', 'y'];
-    List<Color> colors = [
-      Colors.deepOrange[900], Colors.deepOrange[800], Colors.deepOrange[700],
-      Colors.red[900], Colors.red[800], Colors.red[700],
-      Colors.pink[900], Colors.pink[800], Colors.pink[700],
-      Colors.amber[900], Colors.amber[800], Colors.amber[700],
-    ];
-    List<Widget> buttons = new List<Widget>();
-    for (var i = 0; i < vowels.length; i++){
+/*    List<Widget> buttons = new List<Widget>();
+    for (var i = 0; i < vowels.length; i++) {
       buttons.add(buildButton(character: vowels[i], color: colors[i]));
-    }
+    }*/
     return Scaffold(
       appBar: AppBar(
-        title: Text('Vietnamse have ${vowels.length} vowels.'),
+        title: Text('Vietnamese has ${vowels.length} vowels. Tap to hear.'),
+        backgroundColor: Color(int.parse('0xff53000B')),
       ),
-      body: SafeArea(
-        child: Center(
-          child: GridView.count(
+      body: Center(
+        child: buildLayout(),
+        /*child: GridView.count(
+            shrinkWrap: true,
+            padding: EdgeInsets.all(10),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
             crossAxisCount: 3,
             children: buttons
-          ),
-        ),
+        ),*/
       ),
     );
   }
