ab0a4674c065e990df76b465c56fe046a1100a8c
huydung <contact.huydung@gmail.com>
2020-10-12 16:11:34 +0700

    Bootstrap the YouTube mini app. Minor modification to layout of Couple Match for small devices.

---
+++ flutter_edu_vietnamese/lib/pages/e7_YouTube.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class E7_YouTube extends StatefulWidget {
+  @override
+  _E7_YouTubeState createState() => _E7_YouTubeState();
+}
+
+class _E7_YouTubeState extends State<E7_YouTube> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
